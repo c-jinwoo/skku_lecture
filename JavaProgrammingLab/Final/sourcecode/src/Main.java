38ec567022cd9e3e64d52daba198de843c381c43
c-jinwoo <jihn011258@naver.com>
2023-01-12 00:49:30 +0900

    Java Programming Lab

--- JavaProgrammingLab/Final/sourcecode/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JDialog;
-
-public class Main {
-	public static void main(String[] args) {
-		UserLoginForm loginForm = new UserLoginForm();
-		loginForm.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		loginForm.setVisible(true);
-	}
-}
